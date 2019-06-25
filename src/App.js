abd167941f4a0123bfe4393539fa6a408664ccde
Bikram <you@example.com>
2019-06-25 18:11:14 +0530

    sample create react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Dashboard from './routes/dashboard/components/Dashboard';
+
+function App() {
+  return (
+    <Dashboard/>
+  );
+}
+
+export default App;
---
+++ src/routes/dashboard/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../../assets/images/logo.png';
+import '../../../assets/stylesheets/App.css';
+
+const  Dashboard = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/routes/dashboard/components/.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Dashboard;
